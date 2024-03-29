7f36ad5eb8e052da0d3b4ae13f886262d5940841
KanaryCoin <kanary.polkadot@gmail.com>
2024-03-29 23:32:14 +0100

    actu4

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/carlos/Kanary.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/home/carlos/Kanary.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/carlos/Kanary.github.io/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/home/carlos/Kanary.github.io/src/css/custom.css"),
+];
